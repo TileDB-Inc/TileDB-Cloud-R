8451d4ce14b86bfdf7214167bc076a0d5560ae93
Dirk Eddelbuettel <edd@debian.org>
2020-08-07 08:22:45 -0500

    added 'firstCheck.R' demo, short README, keeping original README

--- local/firstCheck.R
+++ local/firstCheck.R
@@ -1,29 +1,4 @@
 
-## tiledbcloud
-
-Work in progress
-
-### How was this built
-
-- using current (2020-Aug-07) Docker container
-  `openapitools/openapi-generator-cli:latest`
-- standard invocation using TileDB Cloud API Spec V1, setting package name
-  and version
-- involing `roxygen2` to build documentation
-
-```sh
-docker run --rm -u 1000:1000 -v $PWD:/work \
-       openapitools/openapi-generator-cli:latest \
-       generate -i /work/openapi-v1.yaml -g r \
-       -p packageName=tiledbcloud -p packageVersion=0.0.1 -o /work/tiledb-cloud-r
-cd tiledb-cloud-r && roxy.r -f   # simple roxygen2 wrapper from littler
-```
-
-### Demo
-
-A few first commands (see [local/](local/) as well)
-
-```r
 library(tiledbcloud)
 
 cl <- ApiClient$new(basePath="https://api.tiledb.com/v1",
@@ -55,4 +30,3 @@ sqlpar <-  SQLParameters$new(name="SomethingToFillInHere",
 ans <- sql$RunSQL("TileDB-Inc", sqlpar)
 
 data.table::rbindlist( ans )            # easiest helper to glue rows together
-```
