c824f2404b8134bd0e7eb985918a736b3d76874c
John Kerl <kerl.john.r@gmail.com>
2021-10-15 12:06:23 -0400

    Docs for execute_udf

--- R/execute.R
+++ R/execute.R
@@ -1,31 +1,52 @@
 ##' TileDB Cloud UDF-Execution Helper
 ##'
-##' TODO: WRITE ME
+##' This function invokes a user-defined function in TileDB Cloud.
+##' Nominally you will first call login(); if not, the results
+##' of the last login at ~/.tiledb/cloud.json will be used.
 ##'
-##' TODO: WRITE ME
+##' The udf and namespace arguments are required; the args argument is
+##' optional.
 ##'
-##' @param udf TODO: WRITE ME
+##' @param udf An R function.
 ##'
-##' @param args TODO: WRITE ME
+##' @param args Arguments to the function. If the function takes
+##' no arguments, this can be omitted. If you want to call by
+##' position, use a list like \code{args=list(123, 456)}. If you want
+##' to call by name, use a named list like \code{args=list(x=123,y=456)}.
 ##'
-##' @param namespace TODO: WRITE ME
+##' @param namespace Namespace within TileDB cloud.
 ##'
-##' @return TODO: WRITE ME
+##' @return The R object which is the return value from the UDF.
 ##' @export
-execute_udf <- function(udf, args, namespace) {
-  # TODO: expose the error condition in which the caller has tried execute without login first
+execute_udf <- function(udf, args=NULL, namespace) {
   client <- .pkgenv[["cl"]] # Expected to be set from login.R
-  # if (is.null(client)) { ... }
+  if (is.null(client)) {
+    stop("tiledbcloud: unable to find login credentials. Please use login().")
+  }
 
   apiudf <- UdfApi$new(client)
   gen_udf <- GenericUDF$new()
   gen_udf$language <- UDFLanguage$new("r")
   gen_udf$exec     <- jsonlite::toJSON(as.integer(serialize(udf, NULL)))
-  gen_udf$argument <- jsonlite::toJSON(as.integer(serialize(args, NULL)))
+  if (!is.null(args)) {
+    gen_udf$argument <- jsonlite::toJSON(as.integer(serialize(args, NULL)))
+  }
 
-  # TODO: handle the case when the return is API-client error
-  resultBytes <- apiudf$SubmitGenericUDF(namespace, gen_udf)
-  resultString = rawToChar(resultBytes)
+  # In the successful case, the result will be an R object.  In a failure case
+  # such as invalid hostname provided to login, it will be an object of type
+  # ApiResponse.
+
+  resultObject <- apiudf$SubmitGenericUDF(namespace, gen_udf)
+  if (typeof(resultObject) != "raw") {
+    # TODO: extract a function to a R/utils.R
+    className <- class(resultObject)[1]
+    if (className == "ApiResponse") {
+      stop(paste("tiledbcloud: received error response:", resultObject$content))
+    } else {
+      stop(paste("tiledbcloud: received error response:", class(resultObject)[1]))
+    }
+  }
+  resultString = rawToChar(resultObject)
   resultJSON = jsonlite::fromJSON(resultString)
   resultValue = resultJSON$value
   resultValue
