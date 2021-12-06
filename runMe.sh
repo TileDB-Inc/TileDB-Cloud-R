67e4b8f286321d411185a7eb6299b1a9ecab1d94
John Kerl <kerl.john.r@gmail.com>
2021-12-06 10:40:38 -0500

    Rename manual-layer files to highlight their role

--- runMe.sh
+++ runMe.sh
@@ -17,16 +17,10 @@ docker run --rm -u 1000:1000 -v $PWD:/work \
 
 echo "** Comparing R/ and tests/testthat/ and man/ (after roxygenize-ing)  [DRY RUNS]"
 rsync --delete \
-  --exclude=init.R \
-  --exclude=configure.R \
-  --exclude=user_layer.R \
-  --exclude=login.R \
-  --exclude=execute.R \
-  --exclude=responses.R \
+  --exclude=manual_layer_*.R \
   -ncav tiledbcloud-generated/R/ R/
 rsync --delete -ncav tiledbcloud-generated/tests/testthat/ tests/testthat/
 ## after roxygen update of man/ inside of tiledb-cloud-r/
 rsync --exclude=login.Rd --exclude=config.Rd --delete -ncav tiledbcloud-generated/man/ man/
-
 
 echo "Done. Remember to adjust api_client for the User field."
