d62ff7a269e66d3ed61e032ae43f9eb487bab64f
Dirk Eddelbuettel <edd@debian.org>
2022-09-16 11:51:30 -0500

    Use GitHub Actions instead of Azure (#114)
    * add new ci.yaml and supporting script
    
    * remove Azure DevOps files
    
    * delete old (outdated) token in json file
    
    * make arrow a suggested package and condition its use
    
    * update and simplify environment variable setup for base and ci uses
    
    * simplify test files as token picked up in configure()
    
    * fix move-and-paste error in DESCRIPTION

--- R/manual_layer_configure.R
+++ R/manual_layer_configure.R
@@ -1,35 +1,4 @@
 
-.loadConfig <- function() {
-    ## should we use rappdirs? follow the XDG_CONFIG_HOME?
-    homedir <- Sys.getenv("HOME")
-    if (homedir == "") {
-        warning("No HOME environment variable.")  # Windows ?
-        return(NULL)
-    }
-    cfgfile <- file.path(homedir, ".tiledb", "cloud.json")
-    if (!file.exists(cfgfile)) {
-        #message("No config file 'cloud.json' found.")
-        return(invisible(NULL))
-    }
-    cfg <- jsonlite::fromJSON(cfgfile)
-}
-
-.storeConfig <- function(homedir=Sys.getenv("HOME")) {
-    if (homedir == "") {
-        stop("No HOME environment variable or homedir value.", call. =FALSE)  # Windows ?
-    }
-    cfgfile <- file.path(homedir, ".tiledb", "cloud.json")
-
-    cfgdata <- jsonlite::toJSON(.pkgenv[["config"]], auto_unbox=TRUE, pretty=TRUE)
-
-    write(cfgdata, cfgfile)
-
-    verbose <- getOption("verbose", "false")
-    if (verbose) {
-      cat("Wrote", cfgfile, "\n")
-    }
-}
-
 ##' Configure TileDB Cloud
 ##'
 ##' Provide the setup configuration for the TileDB Cloud package.
@@ -37,8 +6,6 @@
 ##' It considers four different environment variables:
 ##' \code{TILEDB_REST_TOKEN}, \code{TILEDB_REST_HOST},
 ##' \code{TILEDB_REST_USERNAME}, and \code{TILEDB_REST_PASSWORD}.
-##'
-##' It also reads a configuration file from a file \code{.tiledb/cloud.json}.
 ##'
 ##' To operate, \emph{either} an API token has to be provided and will
 ##' be used, \emph{or} the username and password combination will be
@@ -48,37 +15,18 @@
 ##' @export
 configure <- function() {
 
-    ## Start with environment variables; R returns "" if unset by default
-    token      <- Sys.getenv("TILEDB_REST_TOKEN")
-    host       <- Sys.getenv("TILEDB_REST_HOST")
-    username   <- Sys.getenv("TILEDB_REST_USERNAME")
-    password   <- Sys.getenv("TILEDB_REST_PASSWORD")
+    ## If TILEDB_REST_TOKEN_UNITTEST is supplied (as in CI) it used when no default token
+    ## is set, ditto for the username. This is sufficient for CI runs.  Normal use is enabled
+    ## by either using a token or a username and password (or both).  The host can be unset
+    ## and defaults to the TileDB instance.
+    token      <- Sys.getenv("TILEDB_REST_TOKEN", Sys.getenv("TILEDB_REST_TOKEN_UNITTEST"))
+    host       <- Sys.getenv("TILEDB_REST_HOST", "https://api.tiledb.com")
+    username   <- Sys.getenv("TILEDB_REST_USERNAME", "unittest")
+    password   <- Sys.getenv("TILEDB_REST_PASSWORD", "")
     verify_ssl <- TRUE
-
-    cfg <- .loadConfig()                # load a config file (if one found)
-    if (!is.null(cfg)) {
-        if ("username" %in% names(cfg))  username <- cfg$username
-        if ("password" %in% names(cfg))  password <- cfg$password
-        if (host == "")  host <- gsub("/v1(/)?$", "", cfg$host) # scrubs trailing /v1 or /v1/
-        # If the previous login was without a session-token requested then the cloud-config
-        # JSON file will have username and password but no API key.
-        if (token == "") {
-          if (!is.null(cfg$api_key) && length(cfg$api_key) > 0 && !is.null(cfg$api_key[[1]])) {
-              token <- cfg$api_key[[1]]
-          }
-        }
-        verify_ssl <- cfg$verify_ssl
-    }
 
     ## Check token or username set again?  done in .onAttach
 
-    ## Fallback defaults
-    if (host == "") host <- "https://api.tiledb.com"
-
-    # We use jsonlite to persist sessions. In turn, jsonlite::toJSON will *not*
-    # retain names if the configuration is a named vector. We must use a named
-    # list.  Example: toJSON(c(a=1,b=2)) is '[1,2]' (bad) but
-    # toJSON(list(a=1,b=2),auto_unbox=TRUE) is '{"a":1,"b":2}' (good).
     configuration <- list(api_key    = token,
                           username   = username,
                           password   = password,
