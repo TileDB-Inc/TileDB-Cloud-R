3a210735903cec85938e425441e4ffe1168ef0d0
John Kerl <john.kerl@tiledb.com>
2023-03-22 12:44:04 +0000

    update CI

--- inst/tinytest/test_b_array.R
+++ inst/tinytest/test_b_array.R
@@ -10,7 +10,11 @@ expect_true(is(arr, "ArrayApi"))
 
 res <- arr$GetArrayMetadata("TileDB-Inc", "quickstart_dense")
 expect_true(is(res, "ArrayInfo"))
-expect_equal(res$tiledb_uri, "tiledb://TileDB-Inc/quickstart_dense")
+
+# 2023-03-22 this is now:
+# tiledb://TileDB-Inc/2d6e7def-851e-4832-ae92-202af1f6940d
+# expect_equal(res$tiledb_uri, "tiledb://TileDB-Inc/quickstart_dense")
+
 expect_equal(res$namespace, "TileDB-Inc")
 expect_equal(res$type, "dense")
 #print(str(res))
--- inst/tinytest/test_b_array_info.R
+++ inst/tinytest/test_b_array_info.R
@@ -9,4 +9,6 @@ if (!tiledbcloud:::.logged_in()) exit_file("not logged in")
 info <- tiledbcloud::array_info(namespace="TileDB-Inc", arrayname="quickstart_dense")
 expect_equal(info$namespace, "TileDB-Inc")
 expect_equal(info$name, "quickstart_dense")
-expect_equal(info$tiledb_uri, "tiledb://TileDB-Inc/quickstart_dense")
+# 2023-03-22 this is now:
+# tiledb://TileDB-Inc/2d6e7def-851e-4832-ae92-202af1f6940d
+# expect_equal(info$tiledb_uri, "tiledb://TileDB-Inc/quickstart_dense")
