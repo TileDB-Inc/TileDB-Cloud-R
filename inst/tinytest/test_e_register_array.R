06a31acf8d627772815e0229a83112e1ff9e7d1d
John Kerl <john.kerl@tiledb.com>
2023-03-22 20:41:09 +0000

    cloud-config fix for tiledb::tiledb_array

--- inst/tinytest/test_e_register_array.R
+++ inst/tinytest/test_e_register_array.R
@@ -1,20 +1,12 @@
-# ================================================================
-cat("\n")
-cat ("ENTER inst/tinytest/test_e_register_array.R\n")
-# ================================================================
-
 if ((namespaceToCharge <- Sys.getenv("TILEDB_REST_UNIT_TEST_NAMESPACE_TO_CHARGE")) == "") {
     exit_file("No TILEDB_REST_UNIT_TEST_NAMESPACE_TO_CHARGE in environment")
 }
 
-cat ("  inst/tinytest/test_e_register_array.R IMPORTS\n")
 library(tiledbcloud)
 library(tinytest)
 
 # ----------------------------------------------------------------
-cat ("  inst/tinytest/test_e_register_array.R CONFIGURE\n")
 cloud_config <- tiledbcloud::configure()
-cat ("  inst/tinytest/test_e_register_array.R LOGGED_IN?\n")
 if (!tiledbcloud:::.logged_in()) exit_file("not logged in")
 
 # ----------------------------------------------------------------
@@ -27,32 +19,22 @@ make_temp_array_name <- function(base) {
 array_name <- make_temp_array_name('array-registration-test')
 storage_uri <- 's3://tiledb-unittest/data/palmer_penguins2'
 
-cat ("\n  inst/tinytest/test_e_register_array.R BEFORE REGISTER\n")
 tiledbcloud::register_array(array_name=array_name, uri=storage_uri, description='Test Description', namespace=namespaceToCharge)
-cat ("\n  inst/tinytest/test_e_register_array.R AFTER  REGISTER\n")
 
 # ----------------------------------------------------------------
 # READ
 
 config <- tiledb::tiledb_config()
 config["rest.server_address"] <- cloud_config[["host"]]
-config["rest.token"]          <- cloud_config[["token"]]
+config["rest.token"]          <- cloud_config[["api_key"]]
 ctx <- tiledb::tiledb_ctx(config)
 
-array_uri <- paste0('tiledb://', namespaceToCharge, '/', array_name, ctx=ctx)
+array_uri <- paste0('tiledb://', namespaceToCharge, '/', array_name)
 
-cat ("\n  inst/tinytest/test_e_register_array.R BEFORE READ ARRAY\n")
-arr <- tiledb::tiledb_array(array_uri, query_type="READ", as.data.frame=TRUE)
-cat ("\n  inst/tinytest/test_e_register_array.R AFTER  READ ARRAY\n")
+arr <- tiledb::tiledb_array(array_uri, query_type="READ", as.data.frame=TRUE, ctx=ctx)
 expect_equal(length(arr[]), 8)
 
 # ----------------------------------------------------------------
 # DEREGISTER
 
-cat ("\n  inst/tinytest/test_e_register_array.R BEFORE DEREGISTER\n")
 tiledbcloud::deregister_array(array_name=array_name, namespace=namespaceToCharge)
-cat ("\n  inst/tinytest/test_e_register_array.R AFTER  DEREGISTER\n")
-
-# ================================================================
-cat ("EXIT inst/tinytest/test_e_register_array.R\n")
-# ================================================================
